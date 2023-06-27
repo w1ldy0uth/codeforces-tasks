d1e87d802b4f4f12113ebb0d33854aa54ad0a024
Ivan Shurygin <shurygin1vs@gmail.com>
2023-06-27 11:22:40 +0300

    Initial commit

---
+++ src/main/java/io/w1ldy0uth/codeforces/App.java
@@ -0,0 +1,9 @@
+package io.w1ldy0uth.codeforces;
+
+import io.w1ldy0uth.codeforces.TooLongWords.TooLongWordsTask;
+
+public class App  {
+    public static void main( String[] args ) {
+        TooLongWordsTask.solve();
+    }
+}
---
+++ src/main/java/io/w1ldy0uth/codeforces/TooLongWords/TooLongWordsTask.java
@@ -0,0 +1,30 @@
+package io.w1ldy0uth.codeforces.TooLongWords;
+
+import java.util.*;
+
+public class TooLongWordsTask {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int wordsNumber = sc.nextInt();
+        List<String> output = new ArrayList<String>();
+
+        for (int i = 0; i < wordsNumber; i++) {
+            String word = sc.next();
+            if (word.length() > 10) {
+                output.add(word.charAt(0) + Integer.toString(word.length() - 2) + word.charAt(word.length() - 1));
+            } else {
+                output.add(word);
+            }
+        }
+        sc.close();
+
+        for (String word : output) {
+            System.out.println(word);
+        }
+    }
+
+    // Auxiliary method for running on codeforces.com. Use solve() method for testing 
+    public static void main(String[] args) {
+        solve();
+    }
+}
